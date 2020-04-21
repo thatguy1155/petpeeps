f219068aa9a034d4c923f7a20034568f2afc7c29
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-04-21 15:40:51 +0900

    profile cards

--- dog_biz_finder/src/views/Profile.vue
+++ dog_biz_finder/src/views/Profile.vue
@@ -1,67 +1,83 @@
 <template>
   <div>
-
     <account-options v-show="emptyAccountType" />
     <account />
 
     <v-divider :inset="inset" horizontal color="brown" width="100%"></v-divider>
 
-    <v-layout row justify-center>
+    <!-- <v-flex xs2>
+      <v-card dark color="accent">
+        <v-card-text>
+          <add-biz id="bizButton" />
+        </v-card-text>
+      </v-card>
+    </v-flex>-->
 
-      <!-- <v-flex xs2>
-        <v-card dark color="primary">
+    <!-- <div id="animalContainer"> -->
+      <v-flex> 
+      <v-card class="mx-auto" max-width="434" tile>
+        <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0" cols="12">
+              <v-avatar class="profile" color="grey" size="164" tile>
+                <v-img
+                  src="https://i.pinimg.com/originals/66/95/4f/66954f3cfcb3ec22e7d057bc84059a76.jpg"
+                ></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="py-0">
+              <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="title">Name: Fido</v-list-item-title>
+                  <v-list-item-subtitle>Breed: Puppy</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-card>
+
+      <v-layout row justify-center>
+        <v-flex xs2>
+          <v-card-text>
+            <add-animal id="animalButton" />
+          </v-card-text>
+        </v-flex>
+      </v-layout>
+       </v-flex>
+
+             <v-flex> 
+      <v-card class="mx-auto" max-width="434" tile>
+        <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0" cols="12">
+              <v-avatar class="profile" color="grey" size="164" tile>
+                <v-img
+                  src="https://i.pinimg.com/originals/66/95/4f/66954f3cfcb3ec22e7d057bc84059a76.jpg"
+                ></v-img>
+              </v-avatar>
+            </v-col>
+            <v-col class="py-0">
+              <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                <v-list-item-content>
+                  <v-list-item-title class="title">Name: Fido</v-list-item-title>
+                  <v-list-item-subtitle>Breed: Puppy</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-card>
+
+      <v-layout row justify-center>
+        <v-flex xs2>
           <v-card-text>
             <add-biz id="bizButton" />
           </v-card-text>
-        </v-card>
-      </v-flex>-->
-
-      <!-- <v-flex xs2>
-        <v-card color="white">
-          <v-card-text>two</v-card-text>
-        </v-card>
-      </v-flex>-->
-
-      <v-flex xs2>
-        <v-card dark color="accent">
-          <v-card-text>
-            <add-animal id="animalButton" />
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-
-    <!-- <v-container class="grey lighten-5">
-      <v-row v-for="j in justify" :key="j" :justify="j">
-        <v-col v-for="k in 2" :key="k" md="4">
-          <v-card class="pa-2" outlined tile>
-            <add-animal id="animalButton" />
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>-->
-
-    <v-card class="mx-auto" max-width="434" tile>
-      <v-img height="100%" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
-        <v-row align="end" class="fill-height">
-          <v-col align-self="start" class="pa-0" cols="12">
-            <v-avatar class="profile" color="grey" size="164" tile>
-              <v-img
-                src="https://i.pinimg.com/originals/66/95/4f/66954f3cfcb3ec22e7d057bc84059a76.jpg"
-              ></v-img>
-            </v-avatar>
-          </v-col>
-          <v-col class="py-0">
-            <v-list-item color="rgba(0, 0, 0, .4)" dark>
-              <v-list-item-content>
-                <v-list-item-title class="title">Name: Fido</v-list-item-title>
-                <v-list-item-subtitle>Breed: Puppy</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </v-col>
-        </v-row>
-      </v-img>
-    </v-card>
+        </v-flex>
+      </v-layout>
+       </v-flex>
+    <!-- </div> -->
   </div>
 </template>
 
@@ -69,15 +85,15 @@
 import Account from "@/components/Account";
 import AccountOptions from "@/components/AccountOptions";
 import AddAnimal from "@/components/AddAnimal";
-// import AddBiz from "@/components/AddBiz";
+import AddBiz from "@/components/AddBiz";
 import { mapState, mapActions } from "vuex";
 
 export default {
   components: {
     Account,
     AccountOptions,
-    AddAnimal
-    // AddBiz
+    AddAnimal,
+    AddBiz
   },
 
   data: () => ({
@@ -111,8 +127,19 @@ export default {
 
 <style>
 .v-application--is-ltr .v-responsive__sizer ~ .v-responsive__content {
-    /* margin-left: 100%; */
-    margin-right: 45%;
+  margin-right: 45%;
+}
+#animalContainer {
+  justify-content: space-between;
+  display: inline-flex;
+}
+/* .v-btn:not(.v-btn--round).v-size--default {
+    min-width: 138px;
+    padding: 72px 24px;
+    font-size: 49px;
+} */
+.v-btn.v-size--default {
+  font-size: 0.875rem;
 }
 </style>
 
