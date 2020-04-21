e71a56409d01534f84655bad179d644d6d4874d7
JeremyBeyond <JeremyBeyond@Helens-MacBook-Pro.local>
2020-04-21 12:46:35 +0900

    profile

--- dog_biz_finder/src/views/Profile.vue
+++ dog_biz_finder/src/views/Profile.vue
@@ -6,6 +6,23 @@
     <v-divider :inset="inset" horizontal color="brown" width="100%"></v-divider>
   
     <add-animal id="animalButton"/>
+     <v-layout row justify-center>
+      <v-flex xs2>
+        <v-card dark color="primary">
+          <v-card-text>one</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="secondary">
+          <v-card-text>two</v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs2>
+        <v-card dark color="accent">
+          <v-card-text>three</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
     <add-biz id="bizButton"/>
     
   </div>
