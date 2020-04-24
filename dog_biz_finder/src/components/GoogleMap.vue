32a8c4211863a106787057f10896cdd9baeae7b3
stasonli <stasonli29@gmail.com>
2020-04-24 10:53:21 +0900

    put markers on the map based on search results

--- dog_biz_finder/src/components/GoogleMap.vue
+++ dog_biz_finder/src/components/GoogleMap.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <div>
-      <!-- <h2>Search and add a pin</h2> -->
-      <!-- <label>
-        <gmap-autocomplete
-          @place_changed="setPlace">
-        </gmap-autocomplete>
-        <button @click="addMarker">Search</button>
-      </label> -->
-
-    </div>
     <gmap-map
       :center="center"
       :zoom="14"
@@ -26,43 +16,62 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import {gmapApi} from 'vue2-google-maps'
 
 export default {
   name: "GoogleMap",
   data() {
     return {
-      // default to Seoul
+      // default to Seoul, Gangnam-gu
       center: { lat: 37.532600, lng: 127.024612 },
       markers: [],
       places: [],
       currentPlace: null
     };
   },
-
+  watch: {
+    bizList: function() {
+      this.addMarker()
+    }
+  },
   computed: {
-      google: gmapApi
-    },
+    ...mapState('resultModule', {
+      bizList: state => state.bizList
+    }),
+    google: gmapApi
+  },
   
   mounted() {
     this.geolocate();
   },
 
   methods: {
-    // receives a place object via the autocomplete component
-    setPlace(place) {
-      this.currentPlace = place;
-    },
     addMarker() {
-      if (this.currentPlace) {
-        const marker = {
-          lat: this.currentPlace.geometry.location.lat(),
-          lng: this.currentPlace.geometry.location.lng()
-        };
-        this.markers.push({ position: marker });
-        this.places.push(this.currentPlace);
-        this.center = marker;
-        this.currentPlace = null;
+      let geocoder = new this.google.maps.Geocoder();
+
+      if (this.bizList) {
+        this.markers = []
+
+        for (let i=0; i<this.bizList.length; i++) {
+          console.log('add marker: '+this.bizList[i].address)
+          geocoder.geocode({'address': this.bizList[i].address}, (results, status) => {
+            
+            if (status === 'OK') {
+              let marker = {
+                lat: results[0].geometry.location.lat(),
+                lng: results[0].geometry.location.lng()
+              }
+              this.markers.push({ position: marker })
+
+              this.center = {
+                lat: this.markers[i].position.lat,
+                lng: this.markers[i].position.lng
+              };
+            }
+
+          });
+        }
       }
     },
     geolocate() {
@@ -71,6 +80,7 @@ export default {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
+
       });
     }
   }
