8344e1f7b71263780bb828a016f56f680083d8ff
kimeunhye224 <kimeunhye224@gmail.com>
2020-04-25 14:50:45 +0900

    refactor setting map center position; change which marker in the array to use for position; format doc with prettier

--- dog_biz_finder/src/components/GoogleMap.vue
+++ dog_biz_finder/src/components/GoogleMap.vue
@@ -1,47 +1,43 @@
 <template>
   <div>
-    <gmap-map
-      :center="center"
-      :zoom="14"
-      style="width:100%;  height: 550px;"
-    >
+    <gmap-map :center="center" :zoom="14" style="width:100%;  height: 550px;">
       <gmap-marker
         :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
-        @click="center=m.position"
+        @click="center = m.position"
       ></gmap-marker>
     </gmap-map>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import {gmapApi} from 'vue2-google-maps'
+import { mapState } from "vuex";
+import { gmapApi } from "vue2-google-maps";
 
 export default {
   name: "GoogleMap",
   data() {
     return {
       // default to Seoul, Gangnam-gu
-      center: { lat: 37.532600, lng: 127.024612 },
+      center: { lat: 37.5326, lng: 127.024612 },
       markers: [],
       places: [],
-      currentPlace: null
+      currentPlace: null,
     };
   },
   watch: {
     bizList: function() {
-      this.addMarker()
-    }
+      this.addMarker();
+    },
   },
   computed: {
-    ...mapState('resultModule', {
-      bizList: state => state.bizList
+    ...mapState("resultModule", {
+      bizList: (state) => state.bizList,
     }),
-    google: gmapApi
+    google: gmapApi,
   },
-  
+
   mounted() {
     this.geolocate();
   },
@@ -51,42 +47,41 @@ export default {
       let geocoder = new this.google.maps.Geocoder();
 
       if (this.bizList) {
-        this.markers = []
+        this.markers = [];
 
-        for (let i=0; i<this.bizList.length; i++) {
-          console.log('add marker: '+this.bizList[i].address)
-          geocoder.geocode({'address': this.bizList[i].address}, (results, status) => {
-            
-            if (status === 'OK') {
-              let marker = {
-                lat: results[0].geometry.location.lat(),
-                lng: results[0].geometry.location.lng()
+        for (let i = 0; i < this.bizList.length; i++) {
+          console.log("add marker: " + this.bizList[i].address);
+          geocoder.geocode(
+            { address: this.bizList[i].address },
+            (results, status) => {
+              if (status === "OK") {
+                let marker = {
+                  lat: results[0].geometry.location.lat(),
+                  lng: results[0].geometry.location.lng(),
+                };
+                this.markers.push({ position: marker });
+
+                this.setMapCenter(this.markers[0].position.lat, this.markers[0].position.lng,)
+
               }
-              this.markers.push({ position: marker })
-
-              this.center = {
-                lat: this.markers[i].position.lat,
-                lng: this.markers[i].position.lng
-              };
             }
-
-          });
+          );
         }
       }
     },
     geolocate() {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
-
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setMapCenter(position.coords.latitude, position.coords.longitude)
       });
-    }
-  }
-}
+    },
+    setMapCenter(latCoords, lngCoords) {
+      this.center = {
+        lat: latCoords,
+        lng: lngCoords
+      };
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
