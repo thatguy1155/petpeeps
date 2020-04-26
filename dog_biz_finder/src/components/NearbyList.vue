b640fe6a20c21ab3b0a8813214a806e38385d5d4
kimeunhye224 <kimeunhye224@gmail.com>
2020-04-26 19:17:55 +0900

    add carousel feature for display of biz results (not styled yet)

--- dog_biz_finder/src/components/NearbyList.vue
+++ dog_biz_finder/src/components/NearbyList.vue
@@ -1,40 +1,79 @@
 <template>
-  <v-row id="bizList">
-    <biz-card-item
-      v-for="(biz, index) in bizList"
-      :key="index"
-      :bizName="biz.bizName"
-      :bizType="biz.bizType"
-      :hours="biz.hours"
-      :address="biz.address"
+  <div v-if="bizList.length > 0">
+    <div id="bizList">
+      <bizlist-arrow-button
+        arrowType="left"
+        @click.native="showPrevItem"
+        :disabled="this.reachedMaxLeft"
+      />
+      <biz-card-item
+        class="current-item"
+        :bizName="currentItem.bizName"
+        :bizType="currentItem.bizType"
+        :hours="currentItem.hours"
+        :address="currentItem.address"
+      />
+      <bizlist-arrow-button
+        arrowType="right"
+        @click.native="showNextItem"
+        :disabled="this.reachedMaxRight"
+      />
+    </div>
+    <bizlist-indicators
+      :items="this.bizList"
+      :currentItemIndex="this.currentItemIndex"
+      :showItem="this.showItem"
     />
-  </v-row>
+  </div>
 </template>
 <script>
 import { mapState } from "vuex";
 import BizCardItem from "./componentsWithProps/BizCardItem";
+import BizlistArrowButton from "./componentsWithProps/BizlistArrowButton";
+import BizlistIndicators from "./componentsWithProps/BizlistIndicators";
+
 export default {
   components: {
     BizCardItem,
+    BizlistArrowButton,
+    BizlistIndicators,
   },
   data() {
-    return {};
+    return {
+      currentItemIndex: 0,
+    };
   },
   computed: {
     ...mapState("resultModule", {
       bizList: (state) => state.bizList,
-    })
+    }),
+    currentItem() {
+        return this.bizList[this.currentItemIndex];
+    },
+    reachedMaxLeft() {
+      return this.currentItemIndex === 0;
+    },
+    reachedMaxRight() {
+      return this.currentItemIndex === this.bizList.length - 1;
+    },
   },
-  created() {
-    this.bizList;
+  methods: {
+    showNextItem() {
+      this.currentItemIndex++;
+    },
+    showPrevItem() {
+      this.currentItemIndex--;
+    },
+    showItem(itemIndex) {
+      this.currentItemIndex = itemIndex;
+    },
   },
-  mounted() {},
 };
 </script>
 
 <style>
-  #bizList {
-    z-index: 1;
-    margin-top: -285px;
-  }
+#bizList {
+  z-index: 1;
+  margin-top: -285px;
+}
 </style>
---
+++ dog_biz_finder/src/components/componentsWithProps/BizlistIndicators.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul>
+    <li
+      v-for="(item, index) in items"
+      class="dot"
+      :key="index"
+      :class="{ 'dot-selected': currentItemIndex === index }"
+    >
+      <button
+        class="indicator-btn"
+        @click="showElement(index)"
+        :disabled="currentItemIndex === index"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "BizlistIndicators",
+  props: {
+    items: Array,
+    currentItemIndex: Number,
+    showElement: Function,
+  },
+};
+</script>
+
+<style scoped>
+.dot {
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background: black;
+  opacity: 0.2;
+  cursor: pointer;
+  position: relative;
+}
+
+.dot-selected {
+  opacity: 0.7;
+  cursor: default;
+}
+
+.indicator-btn {
+  position: absolute;
+  left: 0px;
+  padding: 0px;
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background: transparent;
+  border: none;
+}
+</style>
