fcfdf16e025f9f546963dcc3f9e838adc762f50c
YangGang-2020 <themsbun@gmail.com>
2020-04-28 13:47:20 +0900

    put two components into one big component for TotalBiz and Total Animal

--- dog_biz_finder/src/views/Profile.vue
+++ dog_biz_finder/src/views/Profile.vue
@@ -1,57 +1,26 @@
 <template>
   <div>
     <!-- if there is no account type data in the current user, show the component to select a type -->
-
     <account-options v-show="!accountType" />
     <account />
-
-    <!-- ADD A (ANIMAL) MODAL/PROFILE START-->
-    <div class="grid1x1" v-if="accountType === 'personal'">
-      <!-- <div class="grid1x1"> -->
-      <profile-animal />
-      <div>
-        <v-flex xs2>
-          <!-- <v-card-text> -->
-            <add-animal id="animalButton" />
-          <!-- </v-card-text> -->
-        </v-flex>
-      </div>
-    </div>
-    <!-- ADD A (ANIMAL) MODAL/PROFILE END-->
-
-    <!-- ADD A (BUSINESS) MODAL/PROFILE START-->
-    <div class="grid1x1" v-if="accountType === 'business'">
-      <!-- <div class="grid1x1"> -->
-      <profile-biz />
-      <div>
-        <v-flex xs2>
-          <!-- <v-card-text> -->
-            <add-biz id="bizButton" />
-          <!-- </v-card-text> -->
-        </v-flex>
-      </div>
-    </div>
-    <!-- ADD A (BUSINESS) MODAL/PROFILE END-->
+    <total-animal />
+    <total-biz />
   </div>
 </template>
 
 <script>
 import Account from "@/components/Account";
 import AccountOptions from "@/components/AccountOptions";
-import AddAnimal from "@/components/AddAnimal";
-import ProfileAnimal from "@/components/ProfileAnimal";
-import AddBiz from "@/components/AddBiz";
-import ProfileBiz from "@/components/ProfileBiz";
+import TotalAnimal from "@/components/TotalAnimal";
+import TotalBiz from "@/components/TotalBiz";
 import { mapState, mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
     Account,
     AccountOptions,
-    AddAnimal,
-    AddBiz,
-    ProfileAnimal,
-    ProfileBiz
+    TotalAnimal,
+    TotalBiz,
   },
   data: () => {
     return {};
@@ -80,32 +49,5 @@ export default {
 </script>
 
 <style>
-.grid1x1 {
-  min-height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: flex-end;
-}
-.grid1x1 > div {
-  display: flex;
-  flex-basis: calc(52% - 124px);
-  justify-content: center;
-  flex-direction: column;
-}
-#animalButton {
-  height: 165px;
-  width: 371px;
-  min-width: 77px;
-  font-size: 40px;
-  border-radius: 0px;
-}
-#bizButton {
-  height: 165px;
-  width: 371px;
-  min-width: 77px;
-  font-size: 33px;
-  border-radius: 0px;
-}
 </style>
 
