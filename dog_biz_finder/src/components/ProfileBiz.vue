b6e23530465246af5322dbb2e5fe47e405a1d6c2
stasonli <stasonli29@gmail.com>
2020-05-05 11:14:18 +0900

    adding bizInfo to db preparation

--- dog_biz_finder/src/components/ProfileBiz.vue
+++ dog_biz_finder/src/components/ProfileBiz.vue
@@ -1,48 +1,88 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="434" tile>
-      <v-img height="100%" src>
-        <v-row align="end" class="fill-height">
-          <v-col align-self="start" class="pa-0" cols="12">
-            <v-avatar id="profilePic" class="profile" color="grey" size="164" tile>
-              <v-img
-                src="https://favy-inbound-singapore.s3.amazonaws.com/uploads/topic/image/1515/Untitled_design__10_.jpg"
-              ></v-img>
-            </v-avatar>
-            <v-col class="py-0">
-              <v-list-item color="rgba(0, 0, 0, .4)" dark>
-                <v-list-item-content id="bizFont">
-                  <v-list-item-title id="bizFont">Name: Fido's Cafe</v-list-item-title>
-                  <v-list-item-title id="bizFont">Business Type: 24/7</v-list-item-title>
-                  <v-list-item-title id="bizFont">Open time: 24/7</v-list-item-title>
-                  <v-list-item-title id="addressFont">
-                    Address:11 Nonhyeon-ro 151-gil,
-                    <br />Sinsa-dong, Gangnam-gu, Seoul
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-col>
-          </v-col>
-        </v-row>
-      </v-img>
+
+    <v-card
+      class="mx-auto"
+      width="344"
+      outlined
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="overline mb-4">{{petInfo.breed}}</div>
+          <v-list-item-title class="headline mb-1">{{petInfo.name}}</v-list-item-title>
+          <v-list-item-subtitle>I love this pet</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-avatar
+          tile
+          size="80"
+        >
+          <v-img src="https://favy-inbound-singapore.s3.amazonaws.com/uploads/topic/image/1515/Untitled_design__10_.jpg"></v-img>
+        </v-list-item-avatar>
+      </v-list-item>
+
+      <v-card-actions>
+        <EditAnimal v-bind:petInfo="bizInfo"/>
+        <!-- <v-btn text>Button</v-btn> -->
+      </v-card-actions>
     </v-card>
+
+
+    <!-- <div>
+      <v-card class="mx-auto" max-width="434" tile>
+        <v-img height="100%" src>
+          <v-row align="end" class="fill-height">
+            <v-col align-self="start" class="pa-0" cols="12">
+              <v-avatar id="profilePic" class="profile" color="grey" size="164" tile>
+                <v-img
+                  src="https://favy-inbound-singapore.s3.amazonaws.com/uploads/topic/image/1515/Untitled_design__10_.jpg"
+                ></v-img>
+              </v-avatar>
+              <v-col class="py-0">
+                <v-list-item color="rgba(0, 0, 0, .4)" dark>
+                  <v-list-item-content id="bizFont">
+                    <v-list-item-title id="bizFont">Name: Fido's Cafe</v-list-item-title>
+                    <v-list-item-title id="bizFont">Business Type: 24/7</v-list-item-title>
+                    <v-list-item-title id="bizFont">Open time: 24/7</v-list-item-title>
+                    <v-list-item-title id="addressFont">
+                      Address:11 Nonhyeon-ro 151-gil,
+                      <br />Sinsa-dong, Gangnam-gu, Seoul
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-col>
+            </v-col>
+          </v-row>
+        </v-img>
+      </v-card>
+    </div> -->
   </div>
 </template>
 
 <script>
+import EditBiz from "@/components/EditBiz.vue";
+export default {
+    name: "ProfileBiz",
+    props: ["bizInfo"],
+    components: {
+        EditBiz
+    }, 
+}
 </script>
 
 <style>
+#bizFont {
+  color: black;
+  font-size: 41px !important;
+  display: inline-block;
+}
+
 #addressFont {
   color: black;
   font-size: 15px !important;
   display: inline-block;
 }
-#bizFont {
-  color: black;
-  font-size: 20px !important;
-  display: inline-block;
-}
+
 #profilePic {
   float: left;
 }
---
+++ dog_biz_finder/src/store/modules/bizModule.js
@@ -0,0 +1,161 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
+import axios from 'axios'
+
+Vue.use(Vuex);
+
+const state = {
+    bizArray: []   
+};
+
+const mutations = {
+    SET_BIZ(state, payload) {
+        state.bizArray = payload;
+    },
+    //mutation used when people edit their pet info
+    //because state.pets has many pet objects, find the one with the same id as the one you modified
+    //we will need to update this later when we add age
+    UPDATE_ONE_PET(state, payload){
+        state.bizArray.forEach(element => {
+            if(element.id === payload.id){
+                element.name = payload.name,
+                element.breed = payload.breed,
+                element.size = payload.size
+            }
+        });
+    },
+    //mutation ran when you submit the add pet form
+    ADD_ONE_BIZ(state, payload){
+        state.bizArray.push(payload)
+    }
+};
+
+const actions = {
+    getBiz,
+    createBiz,
+    editBiz
+};
+
+const getters = {
+    bizList: (state) => state.bizArray,
+    
+};
+
+//get the user id from db associated with that user id
+//this function is used in getPets() and createPets()
+function getUserId(user) {
+    let uid = user.uid
+    let accountIdResponse = axios.get(`http://dogpeeps?action=getUserInfo&uid=${uid}`)
+        .then(res => {
+            console.log(res.data['id'])
+            return res.data['id']
+        })
+        .catch(err => console.log(err))
+    return accountIdResponse
+
+}
+
+
+
+//Get all of the businesses associated with that user
+async function getBiz({ commit }) {
+    let currUser = firebase.auth().currentUser;
+    const id = await getUserId(currUser)
+    const response = await axios.get(`http://dogpeeps?action=getBiz&id=${id}`);
+    console.log(response.data)
+    commit('SET_BIZ',response.data);//do the mutation below w provided data   
+}
+
+
+
+// //create a biz
+
+async function createBiz({commit}, creationParams) {
+    let currUser = firebase.auth().currentUser;
+    const id = await getUserId(currUser)
+    const params = new URLSearchParams();
+    params.append('action', 'createPet');
+    params.append('id', id);
+    params.append('name', creationParams.name);
+    params.append('breed', creationParams.breed);
+    params.append('size', creationParams.size);
+    await axios.post('http://dogpeeps', params) 
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(err => console.log(err))
+    //after the info has been added to the db, then make a new object in state.pets
+    //this will need to be updated when we implement age
+    commit("ADD_ONE_BIZ",{
+        name:creationParams.name,
+        breed:creationParams.breed,
+        size:creationParams.size
+    })
+
+}
+
+
+//edit biz info
+
+async function editBiz({commit}, creationParams) {
+    const params = new URLSearchParams();
+    params.append('action', 'editPet');
+    params.append('id', creationParams.id);
+    params.append('name', creationParams.name);
+    params.append('breed', creationParams.breed);
+    params.append('size', creationParams.size);
+    await axios.post('http://dogpeeps', params) //)
+        //after the db has the new member, send the user to the home page
+        .then(res => {
+            console.log(res.data)
+            //creationParams.router.push('/');
+        })
+        .catch(err => console.log(err))
+    //update the state after updating the db
+    commit('UPDATE_ONE_BIZ',{
+        id:creationParams.id,
+        name:creationParams.name,
+        breed:creationParams.breed,
+        size:creationParams.size
+    })
+}
+
+// //Delete user's account, after first reauthenticating the user
+// function deleteUser(a, parameters) {
+//     let currUser = firebase.auth().currentUser;
+//     console.log('deleteUser() params', parameters)
+//         //Call reauthenticate, pass on reauthResult(Boolean); if true (reauthentication successful) then delete account
+//     reauthenticate(parameters.currentPw).then((reauthResult) => {
+//         if (reauthResult) {
+//             currUser
+//                 .delete()
+//                 .then(function() {
+//                     const params = new URLSearchParams();
+//                     params.append('action', 'removeAccount');
+//                     params.append('uid', currUser.uid);
+//                     axios.post('http://dogpeeps', params) //)
+//                         .then(res => {
+//                             console.log(res.data)
+//                         })
+//                         .catch(err => console.log(err))
+//                         //After deleting the account, log the user out 
+//                     logout(parameters.router);
+//                 })
+//                 .catch(function(error) {
+//                     console.log("Delete user unsuccessful", error);
+//                 });
+//         }
+//     });
+// }
+
+
+
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+};
