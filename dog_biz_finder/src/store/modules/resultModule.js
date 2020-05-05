119dcc32e33c0c2bfb3dc9c197bade3d690100ca
YangGang-2020 <themsbun@gmail.com>
2020-05-05 15:15:12 +0900

    1. If i refresh the profile page, it gives the users the option to choose between business and personal again when it shouldn't 2. i can update users but cannot add new users 3.I can delete users but i cannot add pets

--- dog_biz_finder/src/store/modules/resultModule.js
+++ dog_biz_finder/src/store/modules/resultModule.js
@@ -63,138 +63,126 @@ export default {
             state.searchedAddr.splice(1, 3);
             commit("CREATE_SEARCHED_ADDRESS", val.searchedGu);
         },
-        actions: {
-            /**
-             * 
-             * @param {obj } { commit } function to call mutation to add gu in searchedAddr in state
-             * @param {obj } { state } to get access to the state.searchAdd to slice the array from 
-             *                         Gu before next search requests
-             * @param {str } val will containt a str of name of Gu selected 
-             */
-            addGu({ commit, state }, val) {
-                state.searchedAddr.splice(1, 3)
-                commit('CREATE_SEARCHED_ADDRESS', val.searchedGu)
-            },
 
-            /**
-             *
-             * @param {obj } { commit } function to call mutation to add dong in searchedAddr in state
-             * @param {obj } { state } to get access to the state.searchAdd to slice the array from
-             *                         Dong before next search requests
-             * @param {str } val will containt a str of name of Dong selected
-             */
-            addDong({ commit, state }, val) {
-                state.searchedAddr.splice(2, 3);
-                commit("CREATE_SEARCHED_ADDRESS", val.searchedDong);
-            },
+        /**
+         *
+         * @param {obj } { commit } function to call mutation to add dong in searchedAddr in state
+         * @param {obj } { state } to get access to the state.searchAdd to slice the array from
+         *                         Dong before next search requests
+         * @param {str } val will containt a str of name of Dong selected
+         */
+        addDong({ commit, state }, val) {
+            state.searchedAddr.splice(2, 3);
+            commit("CREATE_SEARCHED_ADDRESS", val.searchedDong);
+        },
 
-            /**
-             *
-             * @param { obj } { commit } function to call mutation to change state dongList
-             * @param { str } val will containt a str of integers that will correspond with selected gu
-             */
-            getDongList({ commit }, val) {
-                // make request with axios with given code
-                axios
-                    .get(cors_getDongList, {
-                        params: {
-                            menuGubun: "H",
-                            srhType: "LOC",
-                            houseType: "1",
-                            srhYear: "2020",
-                            srhPeriod: "2",
-                            gubunCode: "LAND",
-                            sidoCode: val.searchedGuCode.substring(0, 2),
-                            gugunCode: val.searchedGuCode,
-                        },
-                    })
-                    .then((list) => {
-                        commit("CHANGE_DONG_LIST", list.data.jsonList);
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            },
-
-            /**
-             *
-             * @param {obj } { commit } function to call mutation to change state bizList
-             * @param {obj } { state } to get access to the state.searchAdd to compare with
-             *                         the pre-existing array of addresses for match
-             */
-            getBizList({ commit, state }) {
-                let cleanList = [{
-                        bizName: "Woof Cafe",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 중구 을지로7가 2-1",
+        /**
+         *
+         * @param { obj } { commit } function to call mutation to change state dongList
+         * @param { str } val will containt a str of integers that will correspond with selected gu
+         */
+        getDongList({ commit }, val) {
+            // make request with axios with given code
+            axios
+                .get(cors_getDongList, {
+                    params: {
+                        menuGubun: "H",
+                        srhType: "LOC",
+                        houseType: "1",
+                        srhYear: "2020",
+                        srhPeriod: "2",
+                        gubunCode: "LAND",
+                        sidoCode: val.searchedGuCode.substring(0, 2),
+                        gugunCode: val.searchedGuCode,
                     },
-                    {
-                        bizName: "Safari",
-                        bizType: "Restaurant",
-                        hours: "7:00 ~ 00:00",
-                        address: "서울특별시 중구 을지로6가 18-17",
-                    },
-                    {
-                        bizName: "The Jungle",
-                        bizType: "Restaurant",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 중구 광휘동 58-1",
-                    },
-                    {
-                        bizName: "Animals",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 중구 저동2가 수표로 46 2층",
-                    },
-                    {
-                        bizName: "Sanctuary",
-                        bizType: "Restaurant",
-                        hours: "7:00 ~ 00:00",
-                        address: "서울특별시 중구 명동2가 명동10길 16-1",
-                    },
-                    {
-                        bizName: "Habitat",
-                        bizType: "Restaurant",
-                        hours: "7:00 ~ 00:00",
-                        address: "서울특별시 중구 장충동 동호로 249",
-                    },
-                    {
-                        bizName: "Archipelago",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 동대문구 청량리동 207-42",
-                    },
-                    {
-                        bizName: "Dogs & Friends",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 성북구 안암동5가 산2-1",
-                    },
-                    {
-                        bizName: "People Reserve",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 동대문구 회기동 1-5",
-                    },
-                    {
-                        bizName: "Tropicana",
-                        bizType: "Cafe",
-                        hours: "11:00 ~ 19:00",
-                        address: "서울특별시 강남구 신사동 563-21",
-                    },
-                ];
-                let filteredList = [];
-                let searchedAddrStr = state.searchedAddr.join(" ");
-
-                filteredList = cleanList.filter((item) => {
-                    return item.address.includes(searchedAddrStr);
+                })
+                .then((list) => {
+                    commit("CHANGE_DONG_LIST", list.data.jsonList);
+                })
+                .catch((err) => {
+                    console.log(err);
                 });
+        },
 
-                commit("CHANGE_BIZ_LIST", filteredList);
+        /**
+         *
+         * @param {obj } { commit } function to call mutation to change state bizList
+         * @param {obj } { state } to get access to the state.searchAdd to compare with
+         *                         the pre-existing array of addresses for match
+         */
+        getBizList({ commit, state }) {
+            let cleanList = [{
+                    bizName: "Woof Cafe",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 중구 을지로7가 2-1",
+                },
+                {
+                    bizName: "Safari",
+                    bizType: "Restaurant",
+                    hours: "7:00 ~ 00:00",
+                    address: "서울특별시 중구 을지로6가 18-17",
+                },
+                {
+                    bizName: "The Jungle",
+                    bizType: "Restaurant",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 중구 광휘동 58-1",
+                },
+                {
+                    bizName: "Animals",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 중구 저동2가 수표로 46 2층",
+                },
+                {
+                    bizName: "Sanctuary",
+                    bizType: "Restaurant",
+                    hours: "7:00 ~ 00:00",
+                    address: "서울특별시 중구 명동2가 명동10길 16-1",
+                },
+                {
+                    bizName: "Habitat",
+                    bizType: "Restaurant",
+                    hours: "7:00 ~ 00:00",
+                    address: "서울특별시 중구 장충동 동호로 249",
+                },
+                {
+                    bizName: "Archipelago",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 동대문구 청량리동 207-42",
+                },
+                {
+                    bizName: "Dogs & Friends",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 성북구 안암동5가 산2-1",
+                },
+                {
+                    bizName: "People Reserve",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 동대문구 회기동 1-5",
+                },
+                {
+                    bizName: "Tropicana",
+                    bizType: "Cafe",
+                    hours: "11:00 ~ 19:00",
+                    address: "서울특별시 강남구 신사동 563-21",
+                },
+            ];
+            let filteredList = [];
+            let searchedAddrStr = state.searchedAddr.join(" ");
 
-                //when there is a new biz list, also set the first item on the new list as the selected biz 
-                commit("SET_SELECTED_BIZ", { business: filteredList[0] });
-            }
+            filteredList = cleanList.filter((item) => {
+                return item.address.includes(searchedAddrStr);
+            });
+
+            commit("CHANGE_BIZ_LIST", filteredList);
+
+            //when there is a new biz list, also set the first item on the new list as the selected biz 
+            commit("SET_SELECTED_BIZ", { business: filteredList[0] });
         }
-    };
+    }
+};
